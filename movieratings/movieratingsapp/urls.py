f29f39d3e0fd966fea3304779716fbacc09d6425
TracySanFilipo <tracysanfilipo@gmail.com>
2016-11-18 17:27:59 -0500

    Added views and templates

--- movieratings/movieratingsapp/urls.py
+++ movieratings/movieratingsapp/urls.py
@@ -4,4 +4,9 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^movie/([0-9]+)', views.movie_detail, name='movie'),
+    url(r'^movie_list$', views.movie_list, name='movie_list'),
+    url(r'^rater/([0-9]+)', views.rater_detail, name='rater'),
+    url(r'^rater_list$', views.rater_list, name='rater_list'),
+    url(r'^top_movies$', views.top_20, name='top_movies')
 ]
--- movieratings/movieratingsapp/views.py
+++ movieratings/movieratingsapp/views.py
@@ -1,8 +1,35 @@
 from django.shortcuts import render
-
-# Create your views here.
+from movieratingsapp import models
+from .models import Movie, Rater, Rating
 from django.http import HttpResponse
+from django.db.models import Count, Avg
 
 
 def index(request):
     return HttpResponse("Movie Rating for Older Movies")
+
+
+def movie_detail(request, var):
+    movie = models.Movie.objects.get(pk=var)
+    return render(request, 'movie_detail.html', {'movie':movie})
+
+
+def movie_list(request):
+    movies = models.Movie.objects.all()
+    return render(request, 'movie_list.html', {'movies':movies})
+
+
+def rater_detail(request, var):
+    rater = models.Rater.objects.get(pk=var)
+    all_ratings = rater.rating_set.order_by("rating")
+    return render(request, 'rater_detail.html', {'rater':rater, 'all_ratings': all_ratings})
+
+
+def rater_list(request):
+    raters = models.Rater.objects.all()
+    return render(request, 'rater_list.html', {'raters':raters})
+
+
+def top_20(request):
+    top_movies = models.Movie.objects.annotate(avg_rating=Avg('rating__rating')).order_by('-avg_rating')[:20]
+    return render(request, 'top_movies.html', {'top_movies':top_movies})
